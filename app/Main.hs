b6266c3d3a82fc1a9462ca981ee09017fee9fbe2
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2023-12-08 14:05:00 +0900

    linear haskell example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE LinearTypes #-}
 module Main (main) where
 
-import Lib
+-- import Lib
+
+tuple1 :: Int %1 -> Int %1 -> (Int, Int)
+tuple1 a b = (a, b)
 
 main :: IO ()
-main = someFunc
+main = 
+    let x = tuple1 2 3
+    in print x
+
+-- main = someFunc
+
